22b0cdd3ae75968c4ad2d42c4cab1353bb3e9fd2
munhyunsu <munhyunsu@gmail.com>
2022-03-28 23:10:49 +0900

    Path parameter

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 FLAGS = _ = None
 DEBUG = False
 
@@ -12,13 +14,15 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_arguments('--debug', action='store_true',
-                         help='The present debug message')
-    parser.add_arguments('--exiftool', default='./exiftool/',
-                         help='The directory with exiftool')
+    parser.add_argument('--debug', action='store_true',
+                        help='The present debug message')
+    parser.add_argument('--exiftool', default='./exiftool/',
+                        help='The directory with exiftool')
 
     FLAGS, _ = parser.parse_known_args()
     DEBUG = FLAGS.debug
+
+    FLAGS.exiftool = os.path.abspath(os.path.expanduser(FLAGS.exiftool))
 
     main()
 
