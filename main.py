9897229d2eb694ccefa985ff2a81045766b08401
munhyunsu <munhyunsu@gmail.com>
2022-03-29 08:23:56 +0900

    main

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ FLAGS = _ = None
 DEBUG = False
 
 
+def check_binary():
+    pass
+
+
 def main():
     if DEBUG:
         print(f'Parsed arguments {FLAGS}')
@@ -17,12 +21,18 @@ if __name__ == '__main__':
     parser.add_argument('--debug', action='store_true',
                         help='The present debug message')
     parser.add_argument('--exiftool', default='./exiftool/',
-                        help='The directory with exiftool')
+                        help='The directory for exiftool')
+    parser.add_argument('--input', default='./input.csv',
+                        help='The input csv (fieldnames: filename,altitude,latitude,longitude,roll,yaw,patch)')
+    parser.add_argument('--output', default='./output',
+                        help='The output directory')
 
     FLAGS, _ = parser.parse_known_args()
     DEBUG = FLAGS.debug
 
     FLAGS.exiftool = os.path.abspath(os.path.expanduser(FLAGS.exiftool))
+    FLAGS.input = os.path.abspath(os.path.expanduser(FLAGS.input))
+    FLAGS.output = os.path.abspath(os.path.expanduser(FLAGS.output))
 
     main()
 
